0f7982173f4479e288e5824d3a9270c500322cbd
QQxxx <dominik.suszek@gmail.com>
2023-04-05 14:27:21 +0200

    Entire project rebuilt

--- TPO3_SD_S23396/src/main/java/module-info.java
+++ TPO3_SD_S23396/src/main/java/module-info.java
@@ -1,13 +1,14 @@
-module com.example.tpo3_sd_s23396 {
+module TPO3.SD.S23396 {
+
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
 
+    opens zad1 to javafx.fxml;
+    exports zad1;
 }
